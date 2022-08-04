c9d63162375dba21308414aaac6507440d6f5b10
Xmuluneh <mulabr673@gmail.com>
2022-08-04 11:07:37 +0300

    Draft

--- models/base_model.py
+++ models/base_model.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
-
+"""Module for Base class """
 from datetime import datetime
 import uuid
+from models import storage
 
 
 class BaseModel:
+    """Class for the base model of object hierarchy"""
 
     def __init__(self, *args, **kwargs):
+        """Initialize of the base instance
+        Args:
+            -*args: list of argument
+            -**kwargs:dict of key-values argument
+        """
         if kwargs:
             del kwargs['__class__']
             for key, value in kwargs.items():
@@ -22,12 +29,16 @@ class BaseModel:
             self.updated_at = datetime.now()
 
     def __str__(self):
+        """Return the string representation of an instance"""
         return "[{}] ({}) ({})".format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
+        """Update the update_at attribute with the current datetime."""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
+        """Return the dictionary representation of an instance"""
         my_dict = self.__dict__.copy()
         my_dict['__class__'] = type(self).__name__
         my_dict['created_at'] = my_dict['created_at'].isoformat()
