2e823565d2323321172b6773b348321e86fda180
Xmuluneh <mulabr673@gmail.com>
2022-08-05 10:58:06 +0300

    Draft

---
+++ models/user.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module for user """
+
+
+class User:
+    """class for user"""
+    def __init__(self):
+        """Initialize the public class attribute"""
+        self.email = ''
+        self.password = ''
+        self.first_name = ''
+        self.last_name = ''
+
