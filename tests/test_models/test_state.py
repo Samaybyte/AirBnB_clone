9ba166f269574a02002646a0d5d3623abb3d0b5a
Xmuluneh <mulabr673@gmail.com>
2022-08-07 22:51:45 +0300

    State test case

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Unittest for State Class
+"""
+import contextlib
+from models.state import State
+import unittest
+from datetime import datetime
+from io import StringIO
+
+
+class TestStateClass(unittest.TestCase):
+    def test_classtype(self):
+        """tests class type"""
+        state = State()
+        self.assertIsInstance(state, State)
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
