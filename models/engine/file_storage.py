a4de2c2e9e6c73f95a6b5b9bd5618f1244e45819
Xmuluneh <mulabr673@gmail.com>
2022-08-07 14:06:32 +0300

    bug fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -29,7 +29,26 @@ class FileStorage:
         with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
             obj_data = {k: v.to_dict()
                         for k, v in FileStorage.__objects.items()}
-            json.dump(obj_data, f)
+            json.dump(obj_data, f, indent=4)
+
+    def classes(self):
+        """Returns a dictionary of valid classes and their references."""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
+
+        classes = {"BaseModel": BaseModel,
+                   "User": User,
+                   "State": State,
+                   "City": City,
+                   "Amenity": Amenity,
+                   "Place": Place,
+                   "Review": Review}
+        return classes
 
     def reload(self):
         """Deserializes the JSON file to __objects"""
