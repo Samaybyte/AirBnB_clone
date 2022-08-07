9036b12d48f3ef723166ae9055b437af6da8f769
Samuel Solomon <85625481+Samaybyte@users.noreply.github.com>
2022-08-07 23:06:27 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -5,9 +5,7 @@ from models.base_model import BaseModel
 
 class User:
     """class for user"""
-    def __init__(self):
-        """Initialize the public class attribute"""
-        self.email = ''
-        self.password = ''
-        self.first_name = ''
-        self.last_name = ''
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
