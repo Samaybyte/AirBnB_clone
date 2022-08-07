7445305e9e4c8a100e31fcbadcd6a3959a443d7c
Xmuluneh <mulabr673@gmail.com>
2022-08-07 11:03:03 +0300

    Bug fix

--- models/base_model.py
+++ models/base_model.py
@@ -30,7 +30,7 @@ class BaseModel:
 
     def __str__(self):
         """Return the string representation of an instance"""
-        return "[{}] ({}) ({})".format(type(self).__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
         """Update the update_at attribute with the current datetime."""
